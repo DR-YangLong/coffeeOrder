14da00ab4afb37f614b7130f45ecd69098c4bbeb
杨龙 <410357434@163.com>
2016-04-22 23:38:11 +0800

    complete

--- app/src/main/java/io/github/dr_yanglong/coffeeorder/MainActivity.java
+++ app/src/main/java/io/github/dr_yanglong/coffeeorder/MainActivity.java
@@ -5,76 +5,67 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import java.text.NumberFormat;
-
 public class MainActivity extends AppCompatActivity {
-
-    /**
-     * 初始咖啡数量
-     */
-    private Integer num = 0;
-    /**
-     * 默认咖啡单价
-     */
-    private static final int DEFAULT_PRICE = 6;
-    /**
-     * 咖啡价格
-     */
-    private static int price = DEFAULT_PRICE;
+    private Integer a_score = 0;
+    private Integer b_score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = (TextView) findViewById(R.id.coffee_price);
-        textView.setText("Please select coffee num!");
     }
 
     /**
-     * 将coffee数量显示在view中
+     * 显示分数
      *
-     * @param num 数量
+     * @param score 分数
+     * @param id    view的id
      */
-    private void displayNum(int num) {
-        TextView numView = (TextView) findViewById(R.id.coffee_num);
-        numView.setText(num + "");
+    private void displayScore(int score, int id) {
+        TextView view = (TextView) findViewById(id);
+        view.setText(String.valueOf(score));
     }
 
     /**
-     * 减少咖啡数量
+     * 得分
+     *
+     * @param view 调用此方法的视图控件
      */
-    public void minusCoffee(View view) {
-        synchronized (num) {
-            num = --num > 0 ? num : 0;
-            this.displayNum(num);
+    public void plusScore(View view) {
+        //获取到调用此回调函数对应的View的id
+        String id = view.getResources().getResourceName(view.getId());
+        if (id != null && !"".equals(id)) {
+            id=id.substring(id.lastIndexOf("id/")+3,id.length());
+            int score = 0;
+            if (id.contains("1")) {
+                score += 1;
+            } else if (id.contains("2")) {
+                score += 2;
+            } else {
+                score += 3;
+            }
+            //如果是team a计分
+            if (id.contains("a")) {
+                a_score += score;
+                displayScore(a_score, R.id.a_score);
+            }
+            //如果是team b计分
+            if (id.contains("b")) {
+                b_score += score;
+                displayScore(b_score, R.id.b_score);
+            }
         }
     }
 
     /**
-     * 增加咖啡数量
-     */
-    public void plusCoffee(View view) {
-        synchronized (num) {
-            num = ++num >= Integer.MAX_VALUE ? Integer.MAX_VALUE : num;
-            this.displayNum(num);
-        }
-    }
-
-    /*
-     *下单，计算总价
-     */
-    public void order(View view) {
-        int totalPrice = num * price;
-        this.displayPrice(totalPrice);
-    }
-
-    /**
-     * 显示价格
+     * 重置得分
      *
-     * @param price 价格
+     * @param view 重置按钮
      */
-    private void displayPrice(int price) {
-        TextView priceView = (TextView) findViewById(R.id.coffee_price);
-        priceView.setText("Total: "+NumberFormat.getCurrencyInstance().format(price)+"\nTank You!");
+    public void reset(View view) {
+        a_score = 0;
+        b_score = 0;
+        displayScore(a_score, R.id.a_score);
+        displayScore(b_score, R.id.b_score);
     }
 }
